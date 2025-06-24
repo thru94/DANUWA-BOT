72c1f2030a9be201a6bd9fd8d940cdc09eb2ed5c
thru94 <whatsappshehan6@gmail.com>
2025-06-24 16:01:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,8 +19,8 @@ const defaultConfig = {
   GEMINI_API_KEY: "", // Add Gemini API key here
   REMOVE_BG_API_KEY:"", // Add removebg API key here
   WEATHER_API_KEY: "", // Add your weather API key here
-  BOT_OWNER: "94776121326", // Replace your bot owner number here with 94(country code)
-  ownerNumber: ["94776121326"], // Replace your bot owner number here (same as bot owner number)
+  BOT_OWNER: "94 76 382 9464", // Replace your bot owner number here with 94(country code)
+  ownerNumber: ["94784472411"], // Replace your bot owner number here (same as bot owner number)
   AUTO_STATUS_REACT: "true",
   AUTO_STATUS_REPLY: "true",
   AUTO_STATUS_SEEN: "true",
